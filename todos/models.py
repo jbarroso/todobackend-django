759d21da7fcd5025acc3c06b6f45947491b91d89
jbarroso <jbarroso@antevenio.com>
2020-06-27 12:03:18 +0200

    Changing numeric id to uuid type

--- todos/models.py
+++ todos/models.py
@@ -1,6 +1,8 @@
+import uuid
 from django.db import models
 
 class Todos(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=500)
     completed = models.BooleanField(default=False)
     order = models.IntegerField(db_column='position', default=0)
--- todos/urls.py
+++ todos/urls.py
@@ -6,7 +6,7 @@ app_name = 'todos'
 
 urlpatterns = {
     url(r'^$', views.TodoList.as_view(), name='list'),
-    url(r'^(?P<pk>[0-9]+)/$', views.TodoDetail.as_view(), name='details'),
+    url(r'^(?P<pk>.*)/$', views.TodoDetail.as_view(), name='details'),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
